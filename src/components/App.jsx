d96b0a61943fa5f350eb4ae1bbb485fd743dc449
mellah <mallahkais@gmail.com>
2021-04-13 05:33:57 +0100

    1 - script created

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,31 @@
 var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
-      </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
-      </div>
-    </div>
-  </div>
+	<div>
+		<nav className="navbar">
+			<div className="col-md-6 offset-md-3">
+				<div>
+					<h5>
+						<em>hello</em> view goes here
+					</h5>
+				</div>
+			</div>
+		</nav>
+		<div className="row">
+			<div className="col-md-7">
+				<div>
+					<h5>
+						<em>test</em> view goes here
+					</h5>
+				</div>
+			</div>
+			<div className="col-md-5">
+				<div>
+					<h5>
+						<em>test</em> view goes here
+					</h5>
+				</div>
+			</div>
+		</div>
+	</div>
 );
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
